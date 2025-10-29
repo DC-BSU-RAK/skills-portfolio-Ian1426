4e51aaf7a85ac4292666829eedb9a70539e48eb1
Bernard Ian Santella <santellabernardian@gmail.com>
2025-10-29 10:18:23 +0400

    Update
    Ex1

--- Assessment 1 - Skills Portfolio/Ex1/Math-quiz.py
+++ Assessment 1 - Skills Portfolio/Ex1/Math-quiz.py
@@ -1,1 +1,10 @@
-from tkinter import * 
+import tkinter as tk
+
+
+
+class ArithmethicQuiz:
+    def __init__(self, root):
+        self.root = root
+        self.root.title = ("Arithmetic Quiz") 
+        self.root.geometry("500x400")
+        self.root.resizable(False, False) 
